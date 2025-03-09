5627f5a03a719d92de12010657eeae478a8fd858
isamoher <isamoher@hotmail.com>
2025-03-09 10:49:39 +0200

    disabling update password for authenticated users

--- src/features/authentication/useUpdateUser.js
+++ src/features/authentication/useUpdateUser.js
@@ -4,19 +4,19 @@ import toast from "react-hot-toast";
 import { updateCurrentUser } from "../../services/apiAuth";
 
 export function useUpdateUser() {
-  const queryClient = useQueryClient();
+	const queryClient = useQueryClient();
 
-  // Function to edit a cabin (React Query)
-  const { isLoading: isUpdating, mutate: updateUser } = useMutation({
-    // In React Query, mutation function can only receive one element! So we pass an object with as many elements as we need
-    mutationFn: updateCurrentUser,
-    onSuccess: () => {
-      toast.success("User account successfully updated");
-      queryClient.invalidateQueries({
-        queryKey: ["user"],
-      });
-    },
-    onError: err => toast.error(err.message),
-  });
-  return { isUpdating, updateUser };
+	// Function to edit a cabin (React Query)
+	const { isLoading: isUpdating, mutate: updateUser } = useMutation({
+		// In React Query, mutation function can only receive one element! So we pass an object with as many elements as we need
+		mutationFn: updateCurrentUser,
+		onSuccess: () => {
+			toast.success("User account successfully updated");
+			queryClient.invalidateQueries({
+				queryKey: ["user"],
+			});
+		},
+		onError: err => toast.error(err.message),
+	});
+	return { isUpdating, updateUser };
 }
